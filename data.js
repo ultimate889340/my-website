c138b3a04e81636aa9737402deb5e0f1b11fc143
ultimate889340 <ux63977@gmail.com>
2026-09-01 08:12:31 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,4 +1,11 @@
 const contents = [
+   {
+  title: "Susukuh",
+  size: "124 GB",
+  image: "https://s13.gifyu.com/images/bny6P.jpg",
+  link: "https://link-target.net/1148914/hoycPbG6mSEG"
+   }
+    
     {
         title: "Autumren",
         size: "88 GB",
