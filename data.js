f5137326fd99650f4a27347fb3559659289633ed
ultimate889340 <ux63977@gmail.com>
2026-09-01 08:33:11 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,50 +1,73 @@
 const contents = [
   {
+    id: "autumren",
     title: "Autumren",
     size: "88 GB",
     image: "https://s13.gifyu.com/images/bnyRN.jpg",
-    link: "https://lockr.to/ITWftcAb"
+    link: "https://lockr.to/ITWftcAb",
+    views: 0
   },
+
   {
+    id: "breakmebab",
     title: "Breakmebab",
     size: "17 GB",
     image: "https://s13.gifyu.com/images/bnyRH.jpg",
-    link: "https://lockr.to/TcS3wx3m"
+    link: "https://lockr.to/TcS3wx3m",
+    views: 0
   },
+
   {
+    id: "minaash",
     title: "Minaash",
     size: "10 GB",
     image: "https://s13.gifyu.com/images/bnyRT.jpg",
-    link: "https://lockr.to/8IJiuqFJ"
+    link: "https://lockr.to/8IJiuqFJ",
+    views: 0
   },
+
   {
+    id: "kiara-mia",
     title: "Kiara Mia",
     size: "8 GB",
     image: "https://s13.gifyu.com/images/bnyRK.jpg",
-    link: "https://lockr.to/mTsa5iWw"
+    link: "https://lockr.to/mTsa5iWw",
+    views: 0
   },
+
   {
+    id: "misslexa",
     title: "Misslexa",
     size: "99 GB",
     image: "https://s13.gifyu.com/images/bnyRw.jpg",
-    link: "https://lockr.to/JPzqQNWV"
+    link: "https://lockr.to/JPzqQNWV",
+    views: 0
   },
+
   {
+    id: "ambermg-vip",
     title: "Ambermg Vip",
     size: "66 GB",
     image: "https://s13.gifyu.com/images/bnyR3.jpg",
-    link: "https://lockr.to/dNG3hVnA"
+    link: "https://lockr.to/dNG3hVnA",
+    views: 0
   },
+
   {
+    id: "poppiillouizz",
     title: "Poppiillouizz",
     size: "14 GB",
     image: "https://s13.gifyu.com/images/bnyR9.jpg",
-    link: "https://lockr.to/vwqR0sTz"
+    link: "https://lockr.to/vwqR0sTz",
+    views: 0
   },
+
   {
+    id: "zoe-moore",
     title: "Zoe Moore",
     size: "3 GB",
     image: "https://s13.gifyu.com/images/bnyRE.jpg",
-    link: "https://lockr.to/QWIZvDDK"
+    link: "https://lockr.to/QWIZvDDK",
+    views: 0
   }
 ];
