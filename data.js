0be7f7be96982e9c48663596e0a61476534870c7
ultimate889340 <ux63977@gmail.com>
2026-09-01 08:16:03 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,22 +1,20 @@
 const contents = [
-   {
-  title: "Susukuh",
-  size: "124 GB",
-  image: "https://s13.gifyu.com/images/bny6P.jpg",
-  link: "https://link-target.net/1148914/hoycPbG6mSEG"
-   }
-    
-    {
-        title: "Autumren",
-        size: "88 GB",
-        image: "https://s13.gifyu.com/images/bnygD.jpg",
-        link: "LINK_KONTEN_AUTUMREN"
-    },
-
-    {
-        title: "Amau",
-        size: "200 GB",
-        image: "https://s13.gifyu.com/images/bny6P.jpg",
-        link: "https://lockr.to/QWIZvDDK"
-    }
+  {
+    title: "Susukuh",
+    size: "124 GB",
+    image: "https://s13.gifyu.com/images/bny6P.jpg",
+    link: "https://link-target.net/1148914/hoycPbG6mSEG"
+  },
+  {
+    title: "Autumren",
+    size: "88 GB",
+    image: "https://s13.gifyu.com/images/bnygD.jpg",
+    link: "LINK_KONTEN_AUTUMREN"
+  },
+  {
+    title: "Amau",
+    size: "200 GB",
+    image: "https://s13.gifyu.com/images/bny6P.jpg",
+    link: "https://lockr.to/QWIZvDDK"
+  }
 ];
