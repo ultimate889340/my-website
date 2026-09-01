43b37a7ff134aa80fac2c132f2716f0dff00a368
ultimate889340 <ux63977@gmail.com>
2026-09-01 08:05:13 +0800

    Create data.js

---
+++ data.js
@@ -0,0 +1,15 @@
+const contents = [
+    {
+        title: "Autumren",
+        size: "88 GB",
+        image: "https://s13.gifyu.com/images/bnygD.jpg",
+        link: "LINK_KONTEN_AUTUMREN"
+    },
+
+    {
+        title: "Amau",
+        size: "200 GB",
+        image: "https://s13.gifyu.com/images/bny6P.jpg",
+        link: "https://lockr.to/QWIZvDDK"
+    }
+];
